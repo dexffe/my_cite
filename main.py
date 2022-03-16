3970516e356c1d79c17afcf8cb1b89156b98abb9
dexffe <dexffe@gmail.com>
2022-03-16 17:47:14 +0600

    initial commit

--- main.py
+++ main.py
@@ -13,6 +13,5 @@ def index():
 
 
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0', port=5000)
-    serve(app, host='0.0.0.0', port=5000)
-    print(app.run(host='0.0.0.0', port=5000))
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
