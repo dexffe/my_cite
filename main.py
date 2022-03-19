397680e21ef79d3e0a92d1818e729c35091d390c
dexffe <dexffe@gmail.com>
2022-03-19 19:48:59 +0600

    1

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return '''<html>
-            Я это сделал УРАААААААА!
+            Ольга Николаевна, вы лучшая!
             </html>
             '''
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    # app.run(host='0.0.0.0', port=5000)
+    serve(app, host='0.0.0.0', port=5000)
