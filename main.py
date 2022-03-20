17bc6ed8469b86e60c716e51ca0238bbc89a3d16
dexffe <dexffe@gmail.com>
2022-03-20 09:41:11 +0600

    1

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return '''<html>
-            Ольга Николаевна, вы лучшая!
+            УРААААААААААААА!
             </html>
             '''
 
